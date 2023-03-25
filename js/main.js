57c7e0925cc297e5f70289e63ebcdb6f16482cf4
beks <turgunbaaev@gmail.com>
2023-03-25 14:40:10 +0300

    dfvfdvfd

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ openMenu.onclick = function() {
 closeMenu.onclick = function() {
     menuModal.classList.remove('active');
 }
+
+// Accordion:
+const acc = document.getElementsByClassName("accordion-item");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        let panel = this.children[1];
+        let icon = this.children[0].querySelector('span');
+        icon.classList.toggle("active");
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+} 
